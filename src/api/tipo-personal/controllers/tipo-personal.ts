263c5dd29c13e93d217ac59b15ba46a9901a40ee
Raccoon124 <alsanchzbeltran@gmail.com>
2022-11-04 22:43:20 -0600

    primer commit

---
+++ src/api/tipo-personal/controllers/tipo-personal.ts
@@ -0,0 +1,7 @@
+/**
+ * tipo-personal controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tipo-personal.tipo-personal');
---
+++ src/api/tipo-personal/services/tipo-personal.ts
@@ -0,0 +1,7 @@
+/**
+ * tipo-personal service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::tipo-personal.tipo-personal');
